8e80d10850d4f09c533a123c15f7ac250d82ce27
Riaz Uddin <akreation786@gmail.com>
2020-03-06 08:54:18 +0600

    null va undefined inteview

---
+++ nall-vs-undefined.js
@@ -0,0 +1,14 @@
+let lacky;
+console.log(lacky);
+
+function add (one, two){
+    console.log(one + two);
+}
+
+const man = {name:"sallu", age: 29}
+console.log(man.phone);
+
+const number = [2, 43, 5, 4];
+
+console.log(number[6]);
+
---
+++ true-false.js
@@ -0,0 +1,14 @@
+// false value list;
+// 0
+// ""
+//undefince
+//null
+//NaN
+
+const name = ""
+
+if(name){
+    console.log("This number is True")
+}else{
+    console.log("This number is false")
+}
